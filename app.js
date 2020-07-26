d029bab1c68794adeddaad9b63f46f1a4fdbd0f2
Mark Turgunbaev <gotica-2@mail.ru>
2020-07-26 23:24:59 +0200

    url

--- app.js
+++ app.js
@@ -133,28 +133,11 @@ app.use('/api/public/vacancies', vacanciesPublicRoutes)
 app.use('/api/files', filesRouter)
 
 
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use('/', express.static(path.join(__dirname, 'build')))
-//
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-//     })
-// }
-
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'build')))
 
     app.get('*', (req, res) => {
-        console.log(req.host);
-
-
-        if (req.host === 'admin.tri-xolma') {
-            res.sendFile(path.resolve(__dirname, 'buildA'))
-        } else {
-            res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-        }
-
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
 
